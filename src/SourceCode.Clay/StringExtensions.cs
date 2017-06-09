2ef99db292df005d1b1f8e53824b777162acf0d2
Grant Dickinson <grant@k2.com>
2017-06-09 13:16:24 -0700

    StringExtensions

---
+++ src/SourceCode.Clay/StringExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SourceCode.Clay
+{
+    /// <summary>
+    /// Utility functions for <see cref="System.String"/>.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Returns the specified number of characters from the left of a string.
+        /// Tolerates <paramref name="length"/> values that are too large or too small (or negative).
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <param name="length">The length.</param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string Left(this string str, int length)
+        {
+            var len = length <= 0 ? 0 : length;
+
+            if (str == null || str.Length <= len) return str;
+            return str.Substring(0, len);
+        }
+
+        /// <summary>
+        /// Returns the specified number of characters from the right of a string.
+        /// Tolerates <paramref name="length"/> values that are too large or too small (or negative).
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <param name="length">The length.</param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string Right(this string str, int length)
+        {
+            var len = length <= 0 ? 0 : length;
+
+            if (str == null || str.Length <= len) return str;
+            return str.Substring(str.Length - len, len);
+        }
+
+        /// <summary>
+        /// Truncates a string, inserting an ellipsis if necessary, returning a value with the specified total width.
+        /// Tolerates <paramref name="totalWidth"/> values that are too large or too small (or negative).
+        /// It the value is already smaller then <paramref name="totalWidth"/>, the original value is returned.
+        /// Otherwise the value is truncated to the specified <paramref name="totalWidth"/> and the
+        /// final 3 characters are replaced with "...".
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <param name="totalWidth">The total width.</param>
+        /// <returns>The elided string.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string Elide(this string str, int totalWidth)
+        {
+            if (str == null || totalWidth <= 4 || str.Length <= totalWidth) return str;
+
+            // str.Substring(0, width) + "..." incurs multiple allocations
+            // new StringBuilder(str, width) allocates char[width] regardless
+            var ca = str.ToCharArray(0, totalWidth);
+            ca[totalWidth - 3] = '.'; // Actual ellipsis character = ALT+0133
+            ca[totalWidth - 2] = '.';
+            ca[totalWidth - 1] = '.';
+
+            return new string(ca);
+        }
+    }
+}
