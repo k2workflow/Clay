6ca9c26c220c56e88f90b50862628bcb68684d67
Grant Dickinson <grant@k2.com>
2017-06-12 15:55:28 -0700

    Split files

---
+++ src/SourceCode.Clay.Json/JsonStreamReader.Token.cs
@@ -0,0 +1,25 @@
+﻿namespace SourceCode.Clay.Json
+{
+    public sealed partial class JsonStreamReader // .Token
+    {
+        public enum JsonToken : byte
+        {
+            None = 0, // None
+
+            ObjectOpen, // Object
+            ObjectClose,
+
+            ArrayOpen, // Array
+            ArrayClose,
+
+            Colon, // Delimiters
+            Comma,
+
+            String, // Types
+            Number,
+            True,
+            False,
+            Null
+        }
+    }
+}
