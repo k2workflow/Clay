8ae8102b6944172f4593656b76694b57cac1650e
Grant Dickinson <grant@k2.com>
2017-06-12 16:04:54 -0700

    SourceCode.Clay.Text

---
+++ src/SourceCode.Clay.Text.Tests/StringBuilderFixtures.cs
@@ -0,0 +1,120 @@
+﻿using System.Text;
+using Xunit;
+
+namespace SourceCode.Clay.Text.Tests
+{
+    public static class StringBuilderFixtures
+    {
+        [Trait("Type", "Unit")]
+        [Fact]
+        public static void When_append_format_line_1()
+        {
+            var actual = new StringBuilder("A").AppendFormatLine("-{0}-", 123);
+            const string expected = "A-123-\r\n";
+
+            Assert.Equal(expected.Length, actual.Length);
+            Assert.Equal(expected, actual.ToString());
+        }
+
+        [Trait("Type", "Unit")]
+        [Fact]
+        public static void When_append_format_line_2()
+        {
+            var actual = new StringBuilder("A").AppendFormatLine("-{0}-{1}-", 123, false);
+            const string expected = "A-123-False-\r\n";
+
+            Assert.Equal(expected.Length, actual.Length);
+            Assert.Equal(expected, actual.ToString());
+        }
+
+        [Trait("Type", "Unit")]
+        [Fact]
+        public static void When_append_format_line_3()
+        {
+            var actual = new StringBuilder("A").AppendFormatLine("-{0}-{1}-{2}-", 123, false, "X");
+            const string expected = "A-123-False-X-\r\n";
+
+            Assert.Equal(expected.Length, actual.Length);
+            Assert.Equal(expected, actual.ToString());
+        }
+
+        [Trait("Type", "Unit")]
+        [Fact]
+        public static void When_append_format_line_N()
+        {
+            var actual = new StringBuilder("A").AppendFormatLine("-{0}-{1}-{2}-{3}-", 123, false, "X", 'z');
+            const string expected = "A-123-False-X-z-\r\n";
+
+            Assert.Equal(expected.Length, actual.Length);
+            Assert.Equal(expected, actual.ToString());
+        }
+
+        //[Trait("Type", "Unit")]
+        //[Fact]
+        //public static void When_append_builder_tiny()
+        //{
+        //    var actual = new StringBuilder("A").AppendBuilder(new StringBuilder("B"));
+        //    const string expected = "AB";
+
+        //    Assert.Equal(expected.Length, actual.Length);
+        //    Assert.Equal(expected, actual.ToString());
+        //}
+
+        //[Trait("Type", "Unit")]
+        //[Fact]
+        //public static void When_append_builder_medium()
+        //{
+        //    var x = new string('A', StringBuilderCache.Max / 2);
+        //    var y = new string('B', StringBuilderCache.Max / 2);
+        //    var actual = new StringBuilder(x).AppendBuilder(new StringBuilder(y));
+
+        //    Assert.Equal(x.Length + y.Length, actual.Length);
+        //    Assert.Equal(x + y, actual.ToString());
+        //}
+
+        //[Trait("Type", "Unit")]
+        //[Fact]
+        //public static void When_append_builder_large()
+        //{
+        //    var x = new string('A', StringBuilderCache.Max / 2 + 1);
+        //    var y = new string('B', StringBuilderCache.Max / 2 + 1);
+        //    var actual = new StringBuilder(x).AppendBuilder(new StringBuilder(y));
+
+        //    Assert.Equal(x.Length + y.Length, actual.Length);
+        //    Assert.Equal(x + y, actual.ToString());
+        //}
+
+        //[Trait("Type", "Unit")]
+        //[Fact]
+        //public static void When_append_builder_null()
+        //{
+        //    var actual = new StringBuilder("A").AppendBuilder(null);
+        //    const string expected = "A";
+
+        //    Assert.Equal(expected.Length, actual.Length);
+        //    Assert.Equal(expected, actual.ToString());
+        //}
+
+        //[Trait("Type", "Unit")]
+        //[Fact]
+        //public static void When_append_builder_line()
+        //{
+        //    var actual = new StringBuilder("A").AppendBuilderLine(new StringBuilder("B"));
+        //    const string expected = "AB\r\n";
+
+        //    Assert.Equal(expected.Length, actual.Length);
+        //    Assert.Equal(expected, actual.ToString());
+        //}
+
+        //[Trait("Type", "Unit")]
+        //[Fact]
+        //public static void When_append_builder_line_null()
+        //{
+        //    var actual = new StringBuilder("A").AppendBuilderLine(null);
+        //    const string expected = "A\r\n";
+
+        //    Assert.Equal(expected.Length, actual.Length);
+        //    Assert.Equal(expected, actual.ToString());
+        //}
+    }
+}
---
+++ src/SourceCode.Clay.Text/StringBuilderExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+
+namespace SourceCode.Clay.Text
+{
+    public static class StringBuilderExtensions
+    {
+        public static StringBuilder AppendFormatLine(this StringBuilder sb, string format, object arg0)
+            => sb?.AppendFormat(format, arg0).AppendLine();
+
+        public static StringBuilder AppendFormatLine(this StringBuilder sb, string format, object arg0, object arg1)
+            => sb?.AppendFormat(format, arg0, arg1).AppendLine();
+
+        public static StringBuilder AppendFormatLine(this StringBuilder sb, string format, object arg0, object arg1, object arg3)
+            => sb?.AppendFormat(format, arg0, arg1, arg3).AppendLine();
+
+        public static StringBuilder AppendFormatLine(this StringBuilder sb, string format, params object[] args)
+            => sb?.AppendFormat(format, args).AppendLine();
+
+        public static StringBuilder AppendFormatLine(this StringBuilder sb, IFormatProvider provider, string format, object arg0)
+            => sb?.AppendFormat(provider, format, arg0).AppendLine();
+
+        public static StringBuilder AppendFormatLine(this StringBuilder sb, IFormatProvider provider, string format, object arg0, object arg1)
+            => sb?.AppendFormat(provider, format, arg0, arg1).AppendLine();
+
+        public static StringBuilder AppendFormatLine(this StringBuilder sb, IFormatProvider provider, string format, object arg0, object arg1, object arg3)
+            => sb?.AppendFormat(provider, format, arg0, arg1, arg3).AppendLine();
+
+        public static StringBuilder AppendFormatLine(this StringBuilder sb, IFormatProvider provider, string format, params object[] args)
+            => sb?.AppendFormat(provider, format, args).AppendLine();
+
+        //public static StringBuilder AppendBuilder(this StringBuilder x, StringBuilder y)
+        //{
+        //    if (x == null || x.Length == 0) return y;
+        //    if (y == null || y.Length == 0) return x;
+
+        //    var len = x.Length + y.Length;
+
+        //    StringBuilder concat;
+        //    if (len <= StringBuilderCache.Max)
+        //    {
+        //        // Get a fresh/cached builder
+        //        concat = StringBuilderCache.Acquire(len);
+
+        //        // Concat chars from @x
+        //        for (var i = 0; i < x.Length; i++)
+        //            concat.Append(x[i]);
+        //    }
+        //    else
+        //    {
+        //        // Minimize iterative allocations
+        //        x.EnsureCapacity(len);
+        //        concat = x;
+        //    }
+
+        //    // Concat chars from @y
+        //    for (var i = 0; i < y.Length; i++)
+        //        concat.Append(y[i]);
+
+        //    return concat;
+        //}
+
+        //public static StringBuilder AppendBuilderLine(this StringBuilder x, StringBuilder y)
+        //{
+        //    var concat = x.AppendBuilder(y);
+
+        //    return concat?.AppendLine();
+        //}
+    }
+}
