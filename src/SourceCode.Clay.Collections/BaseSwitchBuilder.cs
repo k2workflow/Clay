1cef92a43599b6afb51ccd566f1e51ebac4d5ccb
Grant Dickinson <grant@k2.com>
2017-06-14 14:06:17 -0700

    SwitchBuilder.Normalize should be preemptively run on input dictionary keys

--- src/SourceCode.Clay.Collections/BaseSwitchBuilder.cs
+++ src/SourceCode.Clay.Collections/BaseSwitchBuilder.cs
@@ -12,8 +12,14 @@ namespace SourceCode.Clay.Collections.Generic
     /// <typeparam name="TValue">The type of values.</typeparam>
     internal abstract class BaseSwitchBuilder<TKey, TValue> : IDynamicSwitch<TKey, TValue>
     {
+        #region Fields
+
         private readonly Func<TKey, int> _indexer;
         private readonly IReadOnlyList<TValue> _values;
+
+        #endregion
+
+        #region Constructors
 
         protected BaseSwitchBuilder(IReadOnlyDictionary<TKey, TValue> cases)
         {
@@ -30,14 +36,22 @@ namespace SourceCode.Clay.Collections.Generic
                 foreach (var @case in cases)
                 {
                     list[i] = @case.Value;
-                    dict[@case.Key] = i;
+
+                    // Expression MUST match #1 elsewhere in this class
+                    var normalizedKey = Normalize(@case.Key);
+
+                    dict[normalizedKey] = i;
                     i++;
                 }
             }
 
-            _indexer = Build(dict);
+            _indexer = BuildSwitchExpression(dict);
             _values = list;
         }
+
+        #endregion
+
+        #region IDynamicSwitch
 
         public TValue this[TKey key] => _values[_indexer(key)];
 
@@ -56,6 +70,10 @@ namespace SourceCode.Clay.Collections.Generic
             return true;
         }
 
+        #endregion
+
+        #region Helpers
+
         /// <summary>
         /// Override this method if the keys need to be transformed in some manner before
         /// being compared. For example, changing string keys to their lowercase equivalent.
@@ -65,11 +83,16 @@ namespace SourceCode.Clay.Collections.Generic
         protected virtual TKey Normalize(TKey key) => key;
 
         /// <summary>
+        /// A persistent pointer to the <see cref="Normalize(TKey)"/> method.
+        /// </summary>
+        private static readonly MethodInfo _normalize = typeof(BaseSwitchBuilder<TKey, TValue>).GetMethod(nameof(Normalize), BindingFlags.Instance | BindingFlags.NonPublic);
+
+        /// <summary>
         /// Builds the underlying <see cref="Expression"/> based switch.
         /// </summary>
         /// <param name="cases">The cases to transform into a dynamic switch.</param>
         /// <returns>A lambda that returns an index for a specified key value.</returns>
-        private Func<TKey, int> Build(IReadOnlyDictionary<TKey, int> cases)
+        private Func<TKey, int> BuildSwitchExpression(IReadOnlyDictionary<TKey, int> cases)
         {
             // Return -1 if item is not found (per standard convention for IndexOf())
             var notFound = Expression.Constant(-1);
@@ -85,21 +108,17 @@ namespace SourceCode.Clay.Collections.Generic
             // Define formal parameter
             var formalParam = Expression.Parameter(typeof(TKey), "key");
 
-            // Expression MUST match #1 below
-            var mi = this.GetType().GetMethod(nameof(Normalize), BindingFlags.Instance | BindingFlags.NonPublic);
+            // Expression MUST match #1 elsewhere in this class
             var @this = Expression.Constant(this);
-            var switchValue = Expression.Call(@this, mi, formalParam);
+            var switchValue = Expression.Call(@this, _normalize, formalParam);
 
             // Create <Key, SwitchCase>[] list
             var i = 0;
             var switchCases = new SwitchCase[count];
             foreach (var @case in cases)
             {
-                // Get Key
+                // Get (already normalized) Key
                 var key = @case.Key;
-
-                // Expression MUST match #1 above
-                key = Normalize(key);
 
                 // Create Case Expression
                 var body = Expression.Constant(@case.Value);
@@ -113,5 +132,7 @@ namespace SourceCode.Clay.Collections.Generic
             var lambda = Expression.Lambda<Func<TKey, int>>(switchExpr, formalParam);
             return lambda.Compile();
         }
+
+        #endregion
     }
 }
