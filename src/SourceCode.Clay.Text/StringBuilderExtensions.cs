1fdfc8a811af17250ed5f0bb2e0aa823bb9e0b58
Grant Dickinson <grant@k2.com>
2017-06-14 12:12:12 -0700

    CR Feedback

--- src/SourceCode.Clay.Text/StringBuilderExtensions.cs
+++ src/SourceCode.Clay.Text/StringBuilderExtensions.cs
@@ -3,35 +3,135 @@ using System.Text;
 
 namespace SourceCode.Clay.Text
 {
+    /// <summary>
+    /// Represents <see cref="System.Text.StringBuilder"/> extensions.
+    /// </summary>
     public static class StringBuilderExtensions
     {
+        /// <summary>
+        /// Appends a formatted string (using zero or more format items) to this instance,
+        /// followed by the default line terminator.
+        /// </summary>
+        /// <param name="sb">The <see cref="StringBuilder"/> instance to append to.</param>
+        /// <param name="format">The format string.</param>
+        /// <param name="arg0">The first format argument.</param>
+        /// <returns>A reference to this instance with format appended.</returns>
         public static StringBuilder AppendFormatLine(this StringBuilder sb, string format, object arg0)
-            => sb?.AppendFormat(format, arg0).AppendLine();
+            => sb?
+            .AppendFormat(format, arg0)
+            .AppendLine();
 
+        /// <summary>
+        /// Appends a formatted string (using zero or more format items) to this instance,
+        /// followed by the default line terminator.
+        /// </summary>
+        /// <param name="sb">The <see cref="StringBuilder"/> instance to append to.</param>
+        /// <param name="format">The format string.</param>
+        /// <param name="arg0">The first format argument.</param>
+        /// <param name="arg1">The second format argument.</param>
+        /// <returns>A reference to this instance with format appended.</returns>
         public static StringBuilder AppendFormatLine(this StringBuilder sb, string format, object arg0, object arg1)
-            => sb?.AppendFormat(format, arg0, arg1).AppendLine();
+            => sb?
+            .AppendFormat(format, arg0, arg1)
+            .AppendLine();
 
-        public static StringBuilder AppendFormatLine(this StringBuilder sb, string format, object arg0, object arg1, object arg3)
-            => sb?.AppendFormat(format, arg0, arg1, arg3).AppendLine();
+        /// <summary>
+        /// Appends a formatted string (using zero or more format items) to this instance,
+        /// followed by the default line terminator.
+        /// </summary>
+        /// <param name="sb">The <see cref="StringBuilder"/> instance to append to.</param>
+        /// <param name="format">The format string.</param>
+        /// <param name="arg0">The first format argument.</param>
+        /// <param name="arg1">The second format argument.</param>
+        /// <param name="arg2">The second format argument.</param>
+        /// <returns>A reference to this instance with format appended.</returns>
+        public static StringBuilder AppendFormatLine(this StringBuilder sb, string format, object arg0, object arg1, object arg2)
+            => sb?
+            .AppendFormat(format, arg0, arg1, arg2)
+            .AppendLine();
 
+        /// <summary>
+        /// Appends a formatted string (using zero or more format items) to this instance,
+        /// followed by the default line terminator.
+        /// </summary>
+        /// <param name="sb">The <see cref="StringBuilder"/> instance to append to.</param>
+        /// <param name="format">The format string.</param>
+        /// <param name="args">The format arguments.</param>
+        /// <returns>A reference to this instance with format appended.</returns>
         public static StringBuilder AppendFormatLine(this StringBuilder sb, string format, params object[] args)
-            => sb?.AppendFormat(format, args).AppendLine();
+            => sb?
+            .AppendFormat(format, args)
+            .AppendLine();
 
+        /// <summary>
+        /// Appends a formatted string (using zero or more format items) to this instance,
+        /// followed by the default line terminator.
+        /// </summary>
+        /// <param name="sb">The <see cref="StringBuilder"/> instance to append to.</param>
+        /// <param name="provider">An object that supplies culture-specific formatting information.</param>
+        /// <param name="format">The format string.</param>
+        /// <param name="arg0">The first format argument.</param>
+        /// <returns>A reference to this instance with format appended.</returns>
         public static StringBuilder AppendFormatLine(this StringBuilder sb, IFormatProvider provider, string format, object arg0)
-            => sb?.AppendFormat(provider, format, arg0).AppendLine();
+            => sb?
+            .AppendFormat(provider, format, arg0)
+            .AppendLine();
 
+        /// <summary>
+        /// Appends a formatted string (using zero or more format items) to this instance,
+        /// followed by the default line terminator.
+        /// </summary>
+        /// <param name="sb">The <see cref="StringBuilder"/> instance to append to.</param>
+        /// <param name="provider">An object that supplies culture-specific formatting information.</param>
+        /// <param name="format">The format string.</param>
+        /// <param name="arg0">The first format argument.</param>
+        /// <param name="arg1">The second format argument.</param>
+        /// <returns>A reference to this instance with format appended.</returns>
         public static StringBuilder AppendFormatLine(this StringBuilder sb, IFormatProvider provider, string format, object arg0, object arg1)
-            => sb?.AppendFormat(provider, format, arg0, arg1).AppendLine();
+            => sb?
+            .AppendFormat(provider, format, arg0, arg1)
+            .AppendLine();
 
-        public static StringBuilder AppendFormatLine(this StringBuilder sb, IFormatProvider provider, string format, object arg0, object arg1, object arg3)
-            => sb?.AppendFormat(provider, format, arg0, arg1, arg3).AppendLine();
+        /// <summary>
+        /// Appends a formatted string (using zero or more format items) to this instance,
+        /// followed by the default line terminator.
+        /// </summary>
+        /// <param name="sb">The <see cref="StringBuilder"/> instance to append to.</param>
+        /// <param name="provider">An object that supplies culture-specific formatting information.</param>
+        /// <param name="format">The format string.</param>
+        /// <param name="arg0">The first format argument.</param>
+        /// <param name="arg1">The second format argument.</param>
+        /// <param name="arg2">The second format argument.</param>
+        /// <returns>A reference to this instance with format appended.</returns>
+        public static StringBuilder AppendFormatLine(this StringBuilder sb, IFormatProvider provider, string format, object arg0, object arg1, object arg2)
+            => sb?
+            .AppendFormat(provider, format, arg0, arg1, arg2)
+            .AppendLine();
 
+        /// <summary>
+        /// Appends a formatted string (using zero or more format items) to this instance,
+        /// followed by the default line terminator.
+        /// </summary>
+        /// <param name="sb">The <see cref="StringBuilder"/> instance to append to.</param>
+        /// <param name="provider">An object that supplies culture-specific formatting information.</param>
+        /// <param name="format">The format string.</param>
+        /// <param name="args">The format arguments.</param>
+        /// <returns>A reference to this instance with format appended.</returns>
         public static StringBuilder AppendFormatLine(this StringBuilder sb, IFormatProvider provider, string format, params object[] args)
-            => sb?.AppendFormat(provider, format, args).AppendLine();
+            => sb?
+            .AppendFormat(provider, format, args)
+            .AppendLine();
 
+        /// <summary>
+        /// Appends two <see cref="StringBuilder"/> instances.
+        /// </summary>
+        /// <param name="sb">The <see cref="StringBuilder"/> instance to append to.</param>
+        /// <param name="arg">The <see cref="StringBuilder"/> instance that should be appended to the first.</param>
+        /// <returns>The initial instance, with the second instance appended to it.</returns>
+        /// <exception cref="System.ArgumentNullException">sb</exception>
         public static StringBuilder AppendBuilder(this StringBuilder sb, StringBuilder arg)
         {
-            if (sb == null || sb.Length == 0) return arg;
+            if (sb == null) throw new ArgumentNullException(nameof(sb));
             if (arg == null || arg.Length == 0) return sb;
 
             // Minimize iterative reallocations
@@ -45,11 +145,21 @@ namespace SourceCode.Clay.Text
             return sb;
         }
 
+        /// <summary>
+        /// Appends two <see cref="StringBuilder"/> instances.
+        /// </summary>
+        /// <param name="sb">The <see cref="StringBuilder"/> instance to append to.</param>
+        /// <param name="arg">The <see cref="StringBuilder"/> instance that should be appended to the first.</param>
+        /// <returns>The initial instance, with the second instance appended to it.</returns>
+        /// <exception cref="System.ArgumentNullException">sb</exception>
         public static StringBuilder AppendBuilderLine(this StringBuilder sb, StringBuilder arg)
         {
-            var concat = sb.AppendBuilder(arg);
+            if (sb == null) throw new ArgumentNullException(nameof(sb));
 
-            return concat?.AppendLine();
+            var concat = sb.AppendBuilder(arg);
+            concat.AppendLine();
+
+            return concat;
         }
     }
 }
