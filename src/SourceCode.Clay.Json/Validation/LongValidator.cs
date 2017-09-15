464befd15470ccc1d4641dd797717a88324d0f0a
Grant Dickinson <grant@k2.com>
2017-09-15 10:35:46 -0700

    Clay.Json.Validation

---
+++ src/SourceCode.Clay.Json/Validation/LongValidator.cs
@@ -0,0 +1,111 @@
+﻿namespace SourceCode.Clay.Json.Validation
+{
+    public sealed class LongValidator
+    {
+        #region Properties
+
+        public long? Min { get; }
+
+        public long? Max { get; }
+
+        public bool MinExclusive { get; }
+
+        public bool MaxExclusive { get; }
+
+        public bool Required { get; }
+
+        public long? MultipleOf { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public LongValidator(long? min, long? max, bool minExclusive, bool maxExclusive, bool required, long? multipleOf)
+        {
+            Min = min;
+            if (Min.HasValue && minExclusive)
+                MinExclusive = true;
+
+            Max = max;
+            if (Max.HasValue && maxExclusive)
+                MaxExclusive = true;
+
+            Required = required;
+            MultipleOf = multipleOf;
+        }
+
+        public LongValidator(long? min, long? max, bool required)
+            : this(min, max, false, false, required, null)
+        { }
+
+        public LongValidator(long? min, long? max)
+            : this(min, max, false, false, false, null)
+        { }
+
+        #endregion
+
+        #region Methods
+
+        public bool IsValid(long? value)
+        {
+            // Check Required
+            if (!value.HasValue)
+                return !(Required); // null + optional = true, null + required = false
+
+            // Check Min
+            if (Min.HasValue)
+            {
+                if (MinExclusive)
+                {
+                    if (value.Value <= Min.Value) return false;
+                }
+                else if (value.Value < Min.Value) return false;
+            }
+
+            // Check Max
+            if (Max.HasValue)
+            {
+                if (MaxExclusive)
+                {
+                    if (value.Value >= Max.Value) return false;
+                }
+                else if (value.Value > Max.Value) return false;
+            }
+
+            // MultipleOf
+            if (MultipleOf.HasValue
+                && MultipleOf.Value != 0 // n % 0 == undefined
+                && value.Value != 0) // 0 % n == 0
+            {
+                var zero = value.Value % MultipleOf.Value == 0;
+                if (!zero) return false;
+            }
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            // Use mathematical notation for open/closed boundaries
+
+            // Required
+            var s = (Required ? "Required: " : string.Empty)
+
+            // Min
+            + (MinExclusive ? "(" : "[")
+            + (Min.HasValue ? $"{Min.Value}" : "-∞")
+            + ", "
+
+            // Max
+            + (Max.HasValue ? $"{Max.Value}" : "∞")
+            + (MaxExclusive ? ")" : "]")
+
+            // MultipleOf
+            + (MultipleOf.HasValue && MultipleOf.Value != 0 ? $" (x{MultipleOf.Value})" : string.Empty);
+
+            return s;
+        }
+
+        #endregion
+    }
+}
---
+++ src/SourceCode.Clay.Json/Validation/PatternValidator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace SourceCode.Clay.Json.Validation
+{
+    public sealed class PatternValidator
+    {
+        #region Constants
+
+        public const RegexOptions DefaultOptions = RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.CultureInvariant;
+        public static readonly TimeSpan DefaultTimeout = TimeSpan.FromMilliseconds(500);
+
+        #endregion
+
+        #region Fields
+
+        private readonly Lazy<Regex> _regex;
+
+        #endregion
+
+        #region Properties
+
+        public string Pattern { get; }
+
+        public bool Required { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public PatternValidator(string pattern, bool required, RegexOptions options, TimeSpan timeout)
+        {
+            if (!string.IsNullOrWhiteSpace(pattern)) throw new ArgumentNullException(nameof(pattern));
+
+            Pattern = pattern;
+            Required = required;
+
+            _regex = new Lazy<Regex>(() => new Regex(pattern, options, timeout), LazyThreadSafetyMode.PublicationOnly);
+        }
+
+        public PatternValidator(string pattern, bool required)
+            : this(pattern, required, DefaultOptions, DefaultTimeout)
+        { }
+
+        #endregion
+
+        #region Methods
+
+        public bool IsValid(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return !Required;
+
+            var isMatch = _regex.Value.IsMatch(value);
+
+            Clear(_regex.Value);
+
+            return isMatch;
+        }
+
+        // Heap analysis shows Regex permanently holds onto last input string, which may be large
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        private static void Clear(Regex regex)
+        {
+            regex.IsMatch(string.Empty);
+        }
+
+        public override string ToString()
+            => (Required ? "Required: " : string.Empty) + Pattern;
+
+        #endregion
+    }
+}
