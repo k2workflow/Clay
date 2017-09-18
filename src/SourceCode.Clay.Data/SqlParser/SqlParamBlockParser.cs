75a062f2d15c8823d381b4e703443c916ceb93e6
Grant Dickinson <grant@k2.com>
2017-09-18 15:17:36 -0700

    WIP

--- src/SourceCode.Clay.Data/SqlParser/SqlParamBlockParser.cs
+++ src/SourceCode.Clay.Data/SqlParser/SqlParamBlockParser.cs
@@ -38,7 +38,7 @@ namespace SourceCode.Clay.Data.SqlParser
                 }
 
                 // PROC | PROCEDURE
-                if (!ParseLiteral(tokenizer, "PROCEDURE", "PROC"))
+                if (!ParseLiteral(tokenizer, "PROCEDURE"))
                 {
                     BuildErrorMessage(parseErrors, new SqlTokenInfo(SqlTokenKind.Literal, "PROCEDURE"), tokenizer.Current);
                     return null;
