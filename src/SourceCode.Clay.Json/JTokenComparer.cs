aa5b890052144f1788a7196cb456f8c5ec5295d9
Grant Dickinson <grant@k2.com>
2017-11-10 12:06:15 -0800

    Minor bug, more units

--- src/SourceCode.Clay.Json/JTokenComparer.cs
+++ src/SourceCode.Clay.Json/JTokenComparer.cs
@@ -10,26 +10,11 @@ using System.Collections.Generic;
 
 namespace SourceCode.Clay.Json
 {
-    public sealed class JTokenComparer : IEqualityComparer<JToken>
+    public static class JTokenComparer
     {
         #region Constants
 
         public static IEqualityComparer<JToken> Default { get; } = new JTokenEqualityComparer();
-
-        #endregion
-
-        #region Constructors
-
-        private JTokenComparer()
-        { }
-
-        #endregion
-
-        #region IEqualityComparer
-
-        public bool Equals(JToken x, JToken y) => Default.Equals(x, y);
-
-        public int GetHashCode(JToken obj) => Default.GetHashCode(obj);
 
         #endregion
     }
