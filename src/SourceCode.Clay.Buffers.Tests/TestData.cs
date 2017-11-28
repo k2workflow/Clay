8f41613119fe5164fd673d533ffedd824a1356b6
Grant <grant@jesanna.com>
2017-11-27 17:31:20 -0800

    Move MemoryExtensions into Clay.Buffers (#204)

---
+++ src/SourceCode.Clay.Buffers.Tests/TestData.cs
@@ -0,0 +1,24 @@
+#region License
+
+// Copyright (c) K2 Workflow (SourceCode Technology Holdings Inc.). All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+#endregion
+
+namespace SourceCode.Clay.Buffers.Tests
+{
+    internal static class TestData
+    {
+        #region Constants
+
+        public static readonly string[] List =
+        {
+            "foo",
+            "bar",
+            "baz",
+            "nin"
+        };
+
+        #endregion
+    }
+}
