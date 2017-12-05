991502de20b8b97384431a2f396ff9cdbc43d3a3
Grant Dickinson <grant@k2.com>
2017-12-05 15:28:25 -0800

    SecureString

---
+++ src/SourceCode.Clay.Tests/SecureStringExtensionsTests.cs
@@ -0,0 +1,44 @@
+#region License
+
+// Copyright (c) K2 Workflow (SourceCode Technology Holdings Inc.). All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+#endregion
+
+using System;
+using System.Security;
+using Xunit;
+
+namespace SourceCode.Clay.Tests
+{
+    public static class SecureStringExtensionsTests
+    {
+        #region Methods
+
+        [Trait("Type", "Unit")]
+        [Fact(DisplayName = nameof(When_convert_securestring))]
+        public static void When_convert_securestring()
+        {
+            // Null
+            SecureString actual = null;
+            Assert.Throws<ArgumentNullException>(() => actual.ToUnsecureString());
+
+            // Empty
+            actual = new SecureString();
+            Assert.Equal(string.Empty, actual.ToUnsecureString());
+
+            // Single
+            actual.AppendChar('h');
+            Assert.Equal("h", actual.ToUnsecureString());
+
+            // Multi
+            actual.AppendChar('e');
+            actual.AppendChar('l');
+            actual.AppendChar('L');
+            actual.AppendChar('0');
+            Assert.Equal("helL0", actual.ToUnsecureString());
+        }
+
+        #endregion
+    }
+}
---
+++ src/SourceCode.Clay/SecureStringExtensions.cs
@@ -0,0 +1,47 @@
+#region License
+
+// Copyright (c) K2 Workflow (SourceCode Technology Holdings Inc.). All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+#endregion
+
+using System;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace SourceCode.Clay
+{
+    /// <summary>
+    /// Utility functions for <see cref="SecureString"/>.
+    /// </summary>
+    public static class SecureStringExtensions
+    {
+        #region Methods
+
+        /// <summary>
+        /// Safely converts a <see cref="SecureString"/> to a <see cref="String"/>.
+        /// </summary>
+        /// <param name="ss"></param>
+        /// <returns></returns>
+        public static string ToUnsecureString(this SecureString ss)
+        {
+            if (ss == null) throw new ArgumentNullException(nameof(ss));
+
+            // https://blogs.msdn.microsoft.com/fpintos/2009/06/12/how-to-properly-convert-securestring-to-string/
+            var ptr = IntPtr.Zero;
+            try
+            {
+                ptr = Marshal.SecureStringToGlobalAllocUnicode(ss);
+
+                var str = Marshal.PtrToStringUni(ptr);
+                return str;
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(ptr);
+            }
+        }
+
+        #endregion
+    }
+}
