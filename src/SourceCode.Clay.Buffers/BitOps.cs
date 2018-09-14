84c747e0862eddca403894a0820e8a5c3e91a1d1
Grant Dickinson <grant@k2.com>
2018-09-14 16:25:06 -0700

    Comments

--- src/SourceCode.Clay.Buffers/BitOps.cs
+++ src/SourceCode.Clay.Buffers/BitOps.cs
@@ -1,10 +1,3 @@
-#region License
-
-// Copyright (c) K2 Workflow (SourceCode Technology Holdings Inc.). All rights reserved.
-// Licensed under the MIT License. See LICENSE file in the project root for full license information.
-
-#endregion
-
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
