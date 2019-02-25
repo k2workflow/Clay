67e65ee8e1c86affd9cf694ff7b451017af0d529
Grant Dickinson <grant@k2.com>
2019-02-25 14:15:40 -0800

    Fix comments in partial class

--- src/SourceCode.Clay/BitOps.cs
+++ src/SourceCode.Clay/BitOps.cs
@@ -1,9 +1,4 @@
-#region License
-
-// Copyright (c) K2 Workflow (SourceCode Technology Holdings Inc.). All rights reserved.
-// Licensed under the MIT License. See LICENSE file in the project root for full license information.
-
-#endregion
+// See the license, notes and warnings in the associated partial class.
 
 using System.Runtime.CompilerServices;
 
