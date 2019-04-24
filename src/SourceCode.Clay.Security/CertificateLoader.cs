c9b976a266b3284f3cb0d3e4e30660bdaa5558e6
Grant Dickinson <grant@k2.com>
2019-04-23 19:48:58 -0700

    Add certificate methods

--- src/SourceCode.Clay.Security/CertificateLoader.cs
+++ src/SourceCode.Clay.Security/CertificateLoader.cs
@@ -6,13 +6,16 @@
 #endregion
 
 using System;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
 namespace SourceCode.Clay.Security
 {
     /// <summary>
-    /// Represents <see cref="X509Certificate2"/> extensions.
+    /// Represents <see cref="X509Certificate2"/> utilities and extensions.
     /// </summary>
     public static class CertificateLoader
     {
@@ -22,80 +25,227 @@ namespace SourceCode.Clay.Security
         public const byte Sha1Length = 20 * 2;
 
         /// <summary>
-        /// Load a <see cref="X509Certificate2"/> given its store location and thumbprint.
+        /// Load a <see cref="X509Certificate2"/> given its store location and <paramref name="thumbprint"/>.
         /// </summary>
         /// <param name="storeName">The certificate <see cref="StoreName"/> to use.</param>
         /// <param name="storeLocation">The certificate <see cref="StoreLocation"/> to use.</param>
         /// <param name="thumbprint">The certificate thumbprint to find.</param>
-        /// <param name="validOnly">true to allow only valid certificates to be returned from the search; otherwise, false.</param>
-        /// <returns>If found, the certificate with the specified thumbprint.</returns>
+        /// <param name="validOnly">If set to <c>false</c>, allows an invalid certificate to be loaded.</param>
+        /// <returns>If found, the certificate with the specified details.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="thumbprint"/>
+        /// </exception>
+        /// <exception cref="FormatException">
+        /// <paramref name="thumbprint"/>
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="storeName"/>
+        /// or
+        /// <paramref name="storeLocation"/>
+        /// </exception>
+        /// <exception cref="InvalidOperationException">Certificate not found: {thumbprint} in {storeLocation}/{storeName}</exception>
         public static X509Certificate2 LoadCertificate(StoreName storeName, StoreLocation storeLocation, string thumbprint, bool validOnly)
         {
-            if (string.IsNullOrWhiteSpace(thumbprint)) throw new ArgumentNullException(nameof(thumbprint));
-            if (thumbprint.Length != Sha1Length) throw new FormatException($"Specified thumbprint should be {Sha1Length} characters long.");
-            if (!IsValid(thumbprint[0]) || !IsValid(thumbprint[thumbprint.Length - 1])) throw new FormatException($"Invalid character(s) detected in thumbprint.");
-
-            var certStore = new X509Store(storeName, storeLocation);
-            certStore.Open(OpenFlags.ReadOnly);
-            try
-            {
-                X509Certificate2Collection certCollection = certStore.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, validOnly);
-
-                if (certCollection.Count == 0)
-                    throw new InvalidOperationException($@"Cannot find certificate in {storeName}/{storeLocation} with thumbprint '{thumbprint}'.");
-
-                // By convention we return the first certificate
-                X509Certificate2 certificate = certCollection[0];
+            if (TryLoadCertificate(storeName, storeLocation, thumbprint, validOnly, out X509Certificate2 certificate))
                 return certificate;
-            }
-            finally
-            {
-                certStore.Close();
-            }
+
+            throw new InvalidOperationException($"Certificate not found: {thumbprint} in {storeLocation}/{storeName}");
         }
 
         /// <summary>
-        /// Try to load a <see cref="X509Certificate2"/> given its store location and thumbprint.
+        /// Try load a <see cref="X509Certificate2"/> given its store location and thumbprint.
         /// </summary>
         /// <param name="storeName">The certificate <see cref="StoreName"/> to use.</param>
         /// <param name="storeLocation">The certificate <see cref="StoreLocation"/> to use.</param>
         /// <param name="thumbprint">The certificate thumbprint to find.</param>
         /// <param name="validOnly">true to allow only valid certificates to be returned from the search; otherwise, false.</param>
-        /// <param name="certificate">If found, the certificate with the specified thumbprint.</param>
-        /// <returns>true if the specified certificate was found; otherwise, false.</returns>
+        /// <param name="certificate">If found, the certificate with the specified details.</param>
+        /// <returns>Returns true if the specified certificate was found; otherwise, false.</returns>
         public static bool TryLoadCertificate(StoreName storeName, StoreLocation storeLocation, string thumbprint, bool validOnly, out X509Certificate2 certificate)
         {
+            if (!Enum.IsDefined(typeof(StoreName), storeName)) throw new ArgumentOutOfRangeException(nameof(storeName));
+            if (!Enum.IsDefined(typeof(StoreLocation), storeLocation)) throw new ArgumentOutOfRangeException(nameof(storeLocation));
             if (string.IsNullOrWhiteSpace(thumbprint)) throw new ArgumentNullException(nameof(thumbprint));
             if (thumbprint.Length != Sha1Length) throw new FormatException($"Specified thumbprint should be {Sha1Length} characters long.");
-            if (!IsValid(thumbprint[0]) || !IsValid(thumbprint[thumbprint.Length - 1])) throw new FormatException($"Invalid character(s) detected in thumbprint.");
+            if (!IsHex(thumbprint[0]) || !IsHex(thumbprint[thumbprint.Length - 1])) throw new FormatException($"Invalid character(s) detected in thumbprint.");
 
-            var certStore = new X509Store(storeName, storeLocation);
-            certStore.Open(OpenFlags.ReadOnly);
-            try
+            using (var store = new X509Store(storeName, storeLocation))
             {
-                X509Certificate2Collection certCollection = certStore.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, validOnly);
+                X509Certificate2Collection storeCertificates = null;
+                certificate = null;
 
-                if (certCollection.Count == 0)
+                try
                 {
-                    certificate = null;
-                    return false;
-                }
+                    store.Open(OpenFlags.ReadOnly);
+                    storeCertificates = store.Certificates;
 
-                // By convention we return the first certificate
-                certificate = certCollection[0];
-                return true;
-            }
-            finally
-            {
-                certStore.Close();
+                    X509Certificate2Collection found = storeCertificates.Find(X509FindType.FindByThumbprint, thumbprint, validOnly);
+
+                    if (found == null || found.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    certificate = found[0]; // By convention we return the first certificate
+                    return true;
+                }
+                finally
+                {
+                    DisposeCertificates(storeCertificates, certificate);
+                }
             }
         }
 
         /// <summary>
-        /// Trims all leading/trailing characters that are not alphanumeric.
+        /// Load a <see cref="X509Certificate2"/> given its store location and <paramref name="subject"/>/<paramref name="issuer"/>.
+        /// </summary>
+        /// <param name="storeName">The certificate <see cref="StoreName"/> to use.</param>
+        /// <param name="storeLocation">The certificate <see cref="StoreLocation"/> to use.</param>
+        /// <param name="subject">The subject.</param>
+        /// <param name="issuer">The issuer.</param>
+        /// <param name="validOnly">If set to <c>false</c>, allows an invalid certificate to be loaded.</param>
+        /// <returns>If found, the certificate with the specified details.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="subject"/>
+        /// or
+        /// <paramref name="issuer"/>
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="storeName"/>
+        /// or
+        /// <paramref name="storeLocation"/>
+        /// </exception>
+        /// <exception cref="InvalidOperationException">Certificate not found: {subject} ({issuer}) in {storeLocation}/{storeName}</exception>
+        public static X509Certificate2 LoadCertificate(StoreName storeName, StoreLocation storeLocation, string subject, string issuer, bool validOnly)
+        {
+            if (TryLoadCertificate(storeName, storeLocation, subject, issuer, validOnly, out X509Certificate2 certificate))
+                return certificate;
+
+            throw new InvalidOperationException($"Certificate not found: {subject} ({issuer}) in {storeLocation}/{storeName}");
+        }
+
+        /// <summary>
+        /// Try load a <see cref="X509Certificate2"/> given its store location and <paramref name="subject"/>/<paramref name="issuer"/>.
+        /// </summary>
+        /// <param name="storeName">The certificate <see cref="StoreName"/> to use.</param>
+        /// <param name="storeLocation">The certificate <see cref="StoreLocation"/> to use.</param>
+        /// <param name="subject">The subject.</param>
+        /// <param name="issuer">The issuer.</param>
+        /// <param name="validOnly">If set to <c>false</c>, allows an invalid certificate to be loaded.</param>
+        /// <param name="certificate">If found, the certificate with the specified details.</param>
+        /// <returns>Returns true if the specified certificate was found; otherwise, false.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="subject"/>
+        /// or
+        /// <paramref name="issuer"/>
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="storeName"/>
+        /// or
+        /// <paramref name="storeLocation"/>
+        /// </exception>
+        /// <exception cref="InvalidOperationException">Certificate not found: {subject} ({issuer}) in {storeLocation}/{storeName}</exception>
+        public static bool TryLoadCertificate(StoreName storeName, StoreLocation storeLocation, string subject, string issuer, bool validOnly, out X509Certificate2 certificate)
+        {
+            if (string.IsNullOrWhiteSpace(subject)) throw new ArgumentNullException(nameof(subject));
+            if (string.IsNullOrWhiteSpace(issuer)) throw new ArgumentNullException(nameof(issuer));
+            if (!Enum.IsDefined(typeof(StoreName), storeName)) throw new ArgumentOutOfRangeException(nameof(storeName));
+            if (!Enum.IsDefined(typeof(StoreLocation), storeLocation)) throw new ArgumentOutOfRangeException(nameof(storeLocation));
+
+            using (var store = new X509Store(storeName, storeLocation))
+            {
+                X509Certificate2Collection storeCertificates = null;
+                certificate = null;
+
+                try
+                {
+                    store.Open(OpenFlags.ReadOnly);
+                    storeCertificates = store.Certificates;
+
+                    X509Certificate2Collection found = storeCertificates.Find(X509FindType.FindBySubjectDistinguishedName, subject, validOnly);
+
+                    if (found == null || found.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    certificate = found
+                        .OfType<X509Certificate2>()
+                        .Where(c => StringComparer.Ordinal.Equals(c.Issuer, issuer))
+                        .OrderByDescending(cert => cert.NotAfter)
+                        .FirstOrDefault(); // By convention we return the first certificate
+
+                    return certificate != null;
+                }
+                finally
+                {
+                    DisposeCertificates(storeCertificates, certificate);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Load a <see cref="X509Certificate2"/> given its <paramref name="fileName"/>/<paramref name="password"/>.
+        /// </summary>
+        /// <param name="fileName">The file path of the certificate.</param>
+        /// <param name="password">The certificate password.</param>
+        /// <returns>If found, the certificate with the specified details.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="fileName"/>
+        /// or
+        /// <paramref name="password"/>
+        /// </exception>
+        /// <exception cref="CryptographicException">
+        /// An error with the certificate occurs. For example: The certificate file does
+        /// not exist. The certificate is invalid. The certificate's password is incorrect.
+        /// </exception>
+        //public static X509Certificate2 LoadCertificate(string fileName, SecureString password)
+        //{
+        //    if (string.IsNullOrWhiteSpace(fileName)) throw new ArgumentNullException(nameof(fileName));
+        //    if (password == null) throw new ArgumentNullException(nameof(password));
+
+        //    return new X509Certificate2(fileName, password);
+        //}
+
+        /// <summary>
+        /// Try load a <see cref="X509Certificate2"/> given its <paramref name="fileName"/>/<paramref name="password"/>.
+        /// </summary>
+        /// <param name="fileName">The file path of the certificate.</param>
+        /// <param name="password">The certificate password.</param>
+        /// <param name="certificate">If found, the certificate with the specified details.</param>
+        /// <returns>Returns true if the specified certificate was found; otherwise, false.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="fileName"/>
+        /// or
+        /// <paramref name="password"/>
+        /// </exception>
+        /// <exception cref="CryptographicException">
+        /// An error with the certificate occurs. For example: The certificate file does
+        /// not exist. The certificate is invalid. The certificate's password is incorrect.
+        /// </exception>
+        public static bool TryLoadCertificate(string fileName, SecureString password, out X509Certificate2 certificate)
+        {
+            if (string.IsNullOrWhiteSpace(fileName)) throw new ArgumentNullException(nameof(fileName));
+            if (password == null || !password.IsReadOnly()) throw new ArgumentNullException(nameof(password));
+
+            try
+            {
+                certificate = new X509Certificate2(fileName, password);
+                return true;
+            }
+            catch (CryptographicException)
+            {
+                certificate = null;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Trims all characters that are not hexadecimal.
         /// Truncates the resulting output to 40 characters if it is longer than that.
         /// (If the thumbprint was copied from MMC, it likely contains special characters)
         /// </summary>
+        /// <param name="thumbprint">The raw thumbprint value to clean.</param>
         public static string NormalizeThumbprint(string thumbprint)
         {
             if (string.IsNullOrWhiteSpace(thumbprint))
@@ -111,7 +261,7 @@ namespace SourceCode.Clay.Security
             {
                 var c = thumbprint[j];
 
-                if (!IsValid(c))
+                if (!IsHex(c))
                     continue;
 
                 ch[i++] = c;
@@ -127,9 +277,23 @@ namespace SourceCode.Clay.Security
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static bool IsValid(char c)
+        private static bool IsHex(char c)
             => (c >= '0' && c <= '9') ||
                (c >= 'A' && c <= 'Z') ||
                (c >= 'a' && c <= 'z');
+
+        private static void DisposeCertificates(X509Certificate2Collection foundCertificates, X509Certificate2 except)
+        {
+            if (foundCertificates != null)
+            {
+                foreach (X509Certificate2 certificate in foundCertificates)
+                {
+                    if (except == null || !certificate.Equals(except))
+                    {
+                        certificate.Dispose();
+                    }
+                }
+            }
+        }
     }
 }
